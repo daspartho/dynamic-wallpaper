53461377c3f0f331e8ebbd69093b09865357e836
Partho <parthodas6176@gmail.com>
2022-08-16 01:53:38 +0530

    added function to get weather description

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import geocoder
+import requests
+import os
+from dotenv import load_dotenv
 
-def get_lat_lng():
+load_dotenv()
+
+def get_lat_lon():
     g = geocoder.ip('me')
     return g.latlng
+
+def get_weather(lat, lon):
+    key = os.getenv("WEATHER_API_KEY")
+    payload = {'lat':lat, 'lon':lon, 'appid':key}
+    response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=payload)
+    data = response.json()
+    return data["weather"][0]["main"]
