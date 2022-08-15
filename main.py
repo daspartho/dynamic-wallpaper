f369a7869975b680ea7117229be21eb19507d167
Partho <parthodas6176@gmail.com>
2022-08-16 03:52:11 +0530

    added function to get wallpaper

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import geocoder
 import requests
 import os
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 
@@ -15,3 +16,13 @@ def get_weather(lat, lon):
     response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=payload)
     data = response.json()
     return data["weather"][0]["main"]
+
+def get_wallpaper(weather_desc):
+    ID = os.getenv("UNSPLASH_CLIENT_ID")
+    payload = {'query':f'{weather_desc} wallpaper', 'client_id':ID, 'per_page':30 ,'orientation':'landscape'}
+    response = requests.get('https://api.unsplash.com/search/photos', params=payload)
+    data = response.json()
+    img_url = data['results'][random.randint(0,29)]['links']['download']
+    img_data = requests.get(img_url).content
+    with open('wallpaper.jpg', 'wb') as handler:
+        handler.write(img_data)
